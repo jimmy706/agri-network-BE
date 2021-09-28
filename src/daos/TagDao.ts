34c4a18ef9dae7c620ab98a8bfb9ba1215820a75
maivanday <maivanday90@gmail.com>
2021-09-28 09:47:59 +0700

    add sort name postTag

--- src/daos/TagDao.ts
+++ src/daos/TagDao.ts
@@ -3,7 +3,7 @@ import PostTagModel, {PostTag} from "@entities/PostTag";
 export class TagDao{
 
   async getPostTag(): Promise<PostTag[]>{
-        const result = await PostTagModel.find();
+        const result = await PostTagModel.find().sort({"name":1});
         return result
     }
 }
